c06c3a3f6ff8102769b9421a5f086dace085e8df
Rafael Bandeira Rodrigues <rafaband@gmail.com>
2023-09-30 22:00:35 -0300

    chore: project base

---
+++ src/main/java/br/com/fechaki/carte/CarteServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fechaki.carte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fechaki/carte/CarteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fechaki.carte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
