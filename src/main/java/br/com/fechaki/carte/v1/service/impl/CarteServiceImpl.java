4379474b1310814859f8b27b0b093c69a27fb20e
Rafael Bandeira Rodrigues <rafaband@gmail.com>
2023-10-04 14:33:32 -0300

    refactor: improvements for service errors

--- src/main/java/br/com/fechaki/carte/v1/service/impl/CarteServiceImpl.java
+++ src/main/java/br/com/fechaki/carte/v1/service/impl/CarteServiceImpl.java
@@ -1,5 +1,9 @@
 package br.com.fechaki.carte.v1.service.impl;
 
+import br.com.fechaki.carte.exception.type.CarteNotExistException;
+import br.com.fechaki.carte.exception.type.CarteNotFoundException;
+import br.com.fechaki.carte.exception.type.CarteNotUpdatedException;
+import br.com.fechaki.carte.exception.type.NoActivatedCarteException;
 import br.com.fechaki.carte.repository.CarteRepository;
 import br.com.fechaki.carte.v1.data.entity.CarteEntity;
 import br.com.fechaki.carte.v1.service.CarteService;
@@ -22,17 +26,20 @@ public class CarteServiceImpl implements CarteService {
 
     @Override
     public Mono<CarteEntity> read(String idPlace, String idCarte) {
-        return repository.findOneByDeletedFalseAndIdPlaceAndId(idPlace, idCarte);
+        return repository.findOneByDeletedFalseAndIdPlaceAndId(idPlace, idCarte)
+                .switchIfEmpty(Mono.error(new CarteNotFoundException(idPlace, idCarte)));
     }
 
     @Override
     public Mono<CarteEntity> readCurrent(String idPlace) {
-        return repository.findOneByDeletedFalseAndActivatedTrueAndIdPlace(idPlace);
+        return repository.findOneByDeletedFalseAndActivatedTrueAndIdPlace(idPlace)
+                .switchIfEmpty(Mono.error(new NoActivatedCarteException(idPlace)));
     }
 
     @Override
     public Mono<Page<CarteEntity>> readAll(String idPlace, Pageable pageable) {
         return repository.findAllByDeletedFalseAndIdPlace(idPlace, pageable)
+                .switchIfEmpty(Mono.error(new CarteNotFoundException(idPlace)))
                 .collectList()
                 .zipWith(repository.count())
                 .map(p -> new PageImpl<>(p.getT1(), pageable, p.getT2()));
@@ -42,7 +49,9 @@ public class CarteServiceImpl implements CarteService {
     public Mono<CarteEntity> update(String idPlace, String idCarte, CarteEntity entity) {
         return repository
                 .findOneByDeletedFalseAndIdPlaceAndId(idPlace, idCarte)
+                .switchIfEmpty(Mono.error(new CarteNotExistException(idPlace, idCarte)))
                 .mapNotNull(e -> this.updateEntity(entity, e))
+                .switchIfEmpty(Mono.error(new CarteNotUpdatedException(idPlace, idCarte)))
                 .flatMap(repository::save);
     }
 
@@ -50,6 +59,7 @@ public class CarteServiceImpl implements CarteService {
     public Mono<Void> activate(String idPlace, String idCarte) {
         return repository
                 .activateByIdPlaceAndId(idPlace, idCarte)
+                .switchIfEmpty(Mono.error(new CarteNotExistException(idPlace, idCarte)))
                 .then();
     }
 
@@ -57,6 +67,7 @@ public class CarteServiceImpl implements CarteService {
     public Mono<Void> deactivate(String idPlace, String idCarte) {
         return repository
                 .deactivateByIdPlaceAndId(idPlace, idCarte)
+                .switchIfEmpty(Mono.error(new CarteNotExistException(idPlace, idCarte)))
                 .then();
     }
 
@@ -64,6 +75,7 @@ public class CarteServiceImpl implements CarteService {
     public Mono<Void> delete(String idPlace, String idCarte) {
         return repository
                 .deleteByIdPlaceAndId(idPlace, idCarte)
+                .switchIfEmpty(Mono.error(new CarteNotExistException(idPlace, idCarte)))
                 .then();
     }
 }
